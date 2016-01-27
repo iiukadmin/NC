821cc6138457a549adb126726c4045e02245dd16
iiukadmin <poonja@MacBookRetina.local>
2016-01-27 15:58:10 +0000

    test

--- scripts/init.js
+++ scripts/init.js
@@ -996,7 +996,7 @@ function onNotificationGCM(e) {
 			navigator.notification.confirm(
 	        	e.message,
 	        	function(buttonIndex) {
-		       	 adminLogin(buttonIndex,e.other_data);
+		       	 adminLogin(buttonIndex,e.subtitle);
 			   	},
 			   	'IIUK.org',
 			   	['Cancel','Login']
