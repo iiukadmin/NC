32ae56b5251c4b6e2a0f5a3cfd77c9d676deda96
iiukadmin <poonja@MacBookRetina.local>
2016-01-29 10:10:53 +0000

    test update iOS badge

--- scripts/init.js
+++ scripts/init.js
@@ -537,6 +537,7 @@ module.controller('ContentController',['$scope','$http','$templateCache','$sce',
         DB.getMessageCount(function(err,count){
              $scope.$apply( function() {
                 $scope.hasMessage = count > 0;
+                pushNotification.setApplicationIconBadgeNumber(successHandler, errorHandler, 200);
             });
         });
         
@@ -975,7 +976,7 @@ function onNotificationGCM(e) {
     break;
 
     case 'message':
-		cordova.plugins.notification.badge.set(10);
+		//cordova.plugins.notification.badge.set(10);
         // if this flag is set, this notification happened while we were in the foreground.
         // you might want to play a sound to get the user's attention, throw up a dialog, etc.
         if ( e.foreground )
