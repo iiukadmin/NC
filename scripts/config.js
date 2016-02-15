181b580c7ef3dee952dd23d488b01e45d99ba27a
iiukadmin <poonja@MacBookRetina.local>
2016-02-15 23:29:42 +0100

    testing farid modifications

--- scripts/config.js
+++ scripts/config.js
@@ -1,11 +1,11 @@
 window.AKHB ={
 	config:{
-		// http://www.iiuk.org/Pages/HealthBoard_App/webservice.php
+		// https://www.iiuk.org/Pages/App/webservice.php
 		remoteAddress : 'http://stage.iiuk.homeip.net/Pages/App/webservice.php',
 		debug:true,
 		version:null,
 		timeout:60*1000*30,
-		taskTimeout:1000*30,
+		taskTimeout:1000*5,
 		messageSyncTimeout:1000*30,
 		senderID:'31742222780'
 	}
