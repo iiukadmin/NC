ba02d439189cbb958ac44892c8f20525aafd6035
iiukadmin <poonja@MacBookRetina.local>
2016-03-25 22:14:20 +0000

    testing

--- scripts/config.js
+++ scripts/config.js
@@ -2,7 +2,7 @@ window.AKHB ={
 	config:{
 		// https://www.iiuk.org/Pages/App/webservice.php
 		// http://stage.iiuk.homeip.net/Pages/App/webservice.php
-		remoteAddress : 'https://www.iiuk.org/Pages/App/webservice.php',
+		remoteAddress : 'http://stage.iiuk.homeip.net/Pages/App/webservice.php',
 		debug:true,
 		version:null,
 		timeout:60*1000*30,
