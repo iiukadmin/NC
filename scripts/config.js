dab4e9297afbe225bce7c1fba5c89055aecc5a98
iiukadmin <poonja@MacBookRetina.local>
2016-04-03 16:49:54 +0100

    test

--- scripts/config.js
+++ scripts/config.js
@@ -1,16 +1,16 @@
 window.AKHB ={
 	config:{
 		// https://www.iiuk.org/Pages/App/webservice2.php
-		// http://stage.iiuk.homeip.net/Pages/App/webservice2.php
+		// https://www.iiuk.org/Pages/HealthBoard_App/webservice2.php
 		// http://stage.iiuk.homeip.net/Pages/HealthBoard_App/webservice2.php
 		// http://stage.iiuk.homeip.net/Pages/App/webservice2.php
-		remoteAddress : 'http://stage.iiuk.homeip.net/Pages/App/webservice2.php',
+		remoteAddress : 'http://stage.iiuk.homeip.net/Pages/HealthBoard_App/webservice2.php',
 		debug:true,
 		version:null,
 		timeout:60*1000*30,
 		taskTimeout:1000*5,
 		messageSyncTimeout:1000*30,
 		senderID:'31742222780',
-		application:'iiuk'
+		application:'akhb'
 	}
 };
