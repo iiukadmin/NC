3635068acc8c9bbe331bf1ec93a49eccb1478545
iiukadmin <poonja@MacBookRetina.local>
2016-06-18 11:34:10 +0100

    test date

--- scripts/init.js
+++ scripts/init.js
@@ -962,7 +962,8 @@ $(document).on('click','a',function(e){
 				  // year, month, day [, hour, minute, second, millisecond ]
 				  // beware: month 0 = january, 11 = december
 		 	      // All day event: set the startdate to midnight and set the enddate to midnight the next day
-	              var startDate = new Date(2016,2,15,18,30,0,0,0); 
+	        //      var startDate = new Date(2016,2,15,18,30,0,0,0); 
+	              var startDate = new Date(event['startDate']); 
 				  var endDate = new Date(2016,2,15,19,30,0,0,0);
 				  var title = event['title'];
 				  var eventLocation = event['eventLocation'];
