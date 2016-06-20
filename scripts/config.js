3641a0b572a856b3c4e2211d21023166981ac440
iiukadmin <poonja@MacBookRetina.local>
2016-06-20 16:47:11 +0100

    joint code

--- scripts/config.js
+++ scripts/config.js
@@ -4,6 +4,9 @@ window.AKHB ={
 		// http://stage.iiuk.homeip.net/Pages/App/webservice2.php
 		// http://stage.iiuk.homeip.net/Pages/HealthBoard_App/webservice2.php
 		// http://stage.iiuk.homeip.net/Pages/App/webservice2.php
+		// SenderID IIUK: 31742222780
+		// SenderID myAKHB: 824841663931
+		// Application: iiuk / akhb
 		remoteAddress : 'http://stage.iiuk.homeip.net/Pages/App/webservice2.php',
 		debug:true,
 		version:null,
