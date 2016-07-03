6c2549b74b2062e4def9ae23fe9ffd9ddb7bc29e
iiukadmin <poonja@MacBookRetina.local>
2016-07-03 18:14:13 +0100

    test

--- scripts/init.js
+++ scripts/init.js
@@ -282,8 +282,6 @@ module.controller('LandingPageController',['$scope','$rootScope','$sce','$templa
 
 module.controller('MessageListController',['$scope','$rootScope','$templateCache',function($scope,$rootScope,$templateCache){
     var scope = $scope;
-    scope.nomessages  = true;
-    scope.realmessages  = false;
     scope.nav =  $templateCache.get('navigation');
     scope.messages = [];
     scope.menuClick = function(){
@@ -301,20 +299,7 @@ module.controller('MessageListController',['$scope','$rootScope','$templateCache
             });
         }) 
     }
-    
-    $scope.isEmpty = function (obj) {
-		for (var i in obj) if (obj.hasOwnProperty(i)) return false;
-		return true;
-	};
-
-    
-    var faridTest = DB.getMessageCount(function(err,count){
-             $scope.$apply( function() {
-                $scope.hasMessage = count > 0;
-            });
-        });
-    
-
+   
     if(Auth.isNetworkConnected()){
         DBSync.runMessageSync(loadMessage,true);
     }else{
