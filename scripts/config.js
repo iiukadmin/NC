85760987005ed038ad3615305a0b7c527240db6e
iiukadmin <poonja@MacBookRetina.local>
2016-07-04 11:58:32 +0100

    sync live version
    test live

--- scripts/config.js
+++ scripts/config.js
@@ -7,7 +7,7 @@ window.AKHB ={
 		// SenderID IIUK: 31742222780
 		// SenderID myAKHB: 824841663931
 		// Application: iiuk / akhb
-		remoteAddress : 'http://stage.iiuk.homeip.net/Pages/App/webservice2.php',
+		remoteAddress : 'https://www.iiuk.org/Pages/App/webservice2.php',
 		debug:true,
 		version:null,
 		timeout:60*1000*30,
