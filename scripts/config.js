f40d6c21d328f8ac340c2d9dfecdf633cc10b417
iiukadmin <poonja@MacBookRetina.local>
2016-07-22 20:36:50 +0100

    Ire

--- scripts/config.js
+++ scripts/config.js
@@ -7,13 +7,13 @@ window.AKHB ={
 		// SenderID IIUK: 31742222780
 		// SenderID myAKHB: 824841663931
 		// Application: iiuk / akhb
-		remoteAddress : 'http://messaging.poonja.co.u/Pages/App/webservice2.php',
+		remoteAddress : 'http://stage.iiuk.homeip.net/Pages/App/webservice2.php',
 		debug:true,
 		version:null,
 		timeout:60*1000*30,
 		taskTimeout:1000*5,
 		messageSyncTimeout:1000*30,
 		senderID:'31742222780',
-		application:'boots'
+		application:'iiuk'
 	}
 };
