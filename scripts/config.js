19f8bca29e8d644116494987d6729a0418768098
iiukadmin <poonja@MacBookRetina.local>
2016-07-22 17:22:05 +0100

    new versions

--- scripts/config.js
+++ scripts/config.js
@@ -4,10 +4,8 @@ window.AKHB ={
 		// http://stage.iiuk.homeip.net/Pages/App/webservice2.php
 		// http://stage.iiuk.homeip.net/Pages/HealthBoard_App/webservice2.php
 		// http://stage.iiuk.homeip.net/Pages/App/webservice2.php
-		// SenderID IIUK: 31742222780
-		// SenderID myAKHB: 824841663931
-		// Application: iiuk / akhb
-		remoteAddress : 'http://dev.iiuk.homeip.net/Pages/App/webservice2.php',
+		//remoteAddress : 'http://stage.iiuk.homeip.net/Pages/HealthBoard_App/webservice2.php',
+		remoteAddress : 'http://messaging.org/Pages/App/webservice2.php',
 		debug:true,
 		version:null,
 		timeout:60*1000*30,
