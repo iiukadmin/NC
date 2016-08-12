17f6b6f6a35b223c3ff1211b60c2af1d7bfd8e3c
iiukadmin <poonja@MacBookRetina.local>
2016-08-12 16:08:27 +0100

    fix

--- scripts/config.js
+++ scripts/config.js
@@ -6,7 +6,7 @@ window.AKHB ={
 		// http://stage.iiuk.homeip.net/Pages/App/webservice2.php
 //		remoteAddress : 'https://connect.anzni.org/Pages/App/webservice2.php',
 //		remoteAddress : 'http://dev.iiaz.org/Pages/App/webservice2.php',
-		remoteAddress : 'http://http://stage.iiaz.homeip.net/Pages/App/webservice2.php',
+		remoteAddress : 'http://stage.iiaz.homeip.net/Pages/App/webservice2.php',
 		debug:true,
 		version:null,
 		timeout:60*1000*30,
