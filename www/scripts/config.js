0d05242223369bd22ddc76aba90caa2d761be9c8
iiukadmin <poonja@MacBookRetina.local>
2017-05-04 10:08:58 +0100

    test

--- www/scripts/config.js
+++ www/scripts/config.js
@@ -9,7 +9,7 @@ window.AKHB ={
 		// Application: iiuk / akhb
 		remoteAddress : 'https://www.iiuk.org/Pages/App/webservice2.php',
 		debug:true,
-		version:'1.1.3',
+		version:'1.1.4',
 		timeout:60*1000*30,
 		taskTimeout:1000*5,
 		messageSyncTimeout:1000*30,
