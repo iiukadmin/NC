c2602cca52f21b1c7421202c90629a8523a58a75
iiukadmin <poonja@MacBookRetina.local>
2017-05-25 21:11:10 +0100

    updated version final

--- www/scripts/config.js
+++ www/scripts/config.js
@@ -7,9 +7,9 @@ window.AKHB ={
 		// SenderID IIUK: 31742222780
 		// SenderID myAKHB: 824841663931
 		// Application: iiuk / akhb
-		remoteAddress : 'http://stage.iiuk.homeip.net/Pages/App/webservice2.php',
+		remoteAddress : 'https://www.iiuk.org/Pages/App/webservice2.php',
 		debug:true,
-		version:'1.1.5',
+		version:'1.1.6',
 		timeout:60*1000*30,
 		taskTimeout:1000*5,
 		messageSyncTimeout:1000*30,
