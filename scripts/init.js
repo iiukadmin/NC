3bce5a8d076611ad5c8855b3e9f7d284cd89068e
iiukadmin <iiukadmin@cfr-uk.org>
2018-04-08 14:02:34 +0100

    test

--- scripts/init.js
+++ scripts/init.js
@@ -119,7 +119,7 @@ module.controller('AppController',['$scope','$rootScope','$templateCache',functi
     }
     document.addEventListener('deviceready', function(){
 	    cordova.plugins.notification.badge.set(10);
-	    
+	    alert('hello');
 	    try{
 	    var push = PushNotification.init({ 
 		    "android": {
