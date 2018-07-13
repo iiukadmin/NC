ab8d7e4deeb5387d8ec0bf0fb740cf1da33ac0fb
iiukadmin <iiukadmin@cfr-uk.org>
2018-07-13 16:32:08 +0100

    test

--- www/js/index.js
+++ www/js/index.js
@@ -14,9 +14,9 @@ var app = {
   takePicture: function(){
     CameraPreview.takePicture(function(imgData){
       document.getElementById('originalPicture').src = 'data:image/jpeg;base64,' + imgData;
-      var xhr = new XMLHttpRequest();
-	  xhr.open('GET', "http://poonja.co.uk?hello=1235", true);
-	  alert('asdf');
+     // var xhr = new XMLHttpRequest();
+	 // xhr.open('GET', "http://poonja.co.uk?hello=1235", true);
+	 // alert('asdf');
     });
   },
 
