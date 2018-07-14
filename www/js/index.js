991caafa95a45027d7de06001f3b3e90e511ea94
iiukadmin <iiukadmin@cfr-uk.org>
2018-07-14 08:21:04 +0100

    test

--- www/js/index.js
+++ www/js/index.js
@@ -8,8 +8,8 @@ var app = {
   },
 
   camera: function(){
-    CameraPreview.startCamera({x: 50, y: 50, width: 100, height:100, camera: "front", tapPhoto: true, previewDrag: false, toBack: true});
-    CameraPreview.switchCamera();
+    CameraPreview.startCamera({x: 50, y: 50, width: 100, height: 100, toBack: false, previewDrag: true, tapPhoto: true});
+    //CameraPreview.switchCamera();
   },
 
 
