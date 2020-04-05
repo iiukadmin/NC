5400589f05504250e4573e276afdc090ace226d6
iiukadmin <iiukadmin@cfr-uk.org>
2020-04-05 11:36:36 +0100

    live link

--- scripts/config.js
+++ scripts/config.js
@@ -7,7 +7,7 @@ window.AKHB ={
 		// SenderID myAKHB: 824841663931
 		// Application: iiuk / akhb
 //		remoteAddress : 'https://www.iiuk.org/Pages/App/webservice2.php',
-		remoteAddress : 'http://stage.iiuae.homeip.net/Pages/App/webservice2.php',
+		remoteAddress : 'https://www.iiuae.org/Pages/App/webservice2.php',
 		debug:true,
 		version:'1.1.18',
 		timeout:60*1000*30,
